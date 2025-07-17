6ea18f3cc32398f66b6da660ecb56f85f57181a1
WANGICHEN <149982770+WANGICHEN@users.noreply.github.com>
2025-07-17 10:28:50 +0800

    Add files via upload

---
+++ cdf.py
@@ -0,0 +1,41 @@
+import pandas as pd
+from docx import Document
+
+columns = [
+    'Object/part No.', 'Manufacturer/trademark', 'Type/model',
+    'Technical data', 'Standard', 'Mark(s) of conformity'
+]
+def save_cdf_to_word(df):
+    # 建立一個 Word 文件
+    doc = Document()
+    # 加入表格：rows 為資料列數 + 標題列, cols 為欄位數
+    table = doc.add_table(rows=1, cols=len(columns))
+    table.style = 'Table Grid'
+
+    # 寫入欄位標題
+    hdr_cells = table.rows[0].cells
+    for i, column in enumerate(columns):
+        hdr_cells[i].text = str(column)
+
+    for index, row in df.iterrows():
+        row_cells = table.add_row().cells
+        for i, col in enumerate(columns):
+            value = row[col] if col in row else ""
+            row_cells[i].text = str(value)
+
+    return doc
+
+
+def run(cdf_path):
+    # 讀取 Excel 檔案
+    df = pd.read_excel('CDF_database_2025.03.27_Chris.xlsx')
+    cdf_df = pd.read_excel(cdf_path)
+    cdf_df = cdf_df.merge(
+        df[['Manufacturer/trademark', 'Type/model',
+        'Technical data', 'Standard']],
+        on=cdf_df.columns.to_list(),
+        how='left'
+        )
+
+    doc = save_cdf_to_word(cdf_df)
+    return doc
---
+++ streamlit_app_cdf.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import os
+import cdf
+import tempfile
+
+st.title("PDF → Word 自動轉換工具")
+
+# 上傳 PDF
+cdf_file = st.file_uploader("請上傳 CDF 檔案", type=["xlsx"])
+
+if cdf_file:
+    # 檢查檔案是否為 Excel 檔案
+    if not cdf_file.name.endswith(".xlsx"): 
+        st.error("請上傳一個有效的 Excel 檔案 (.xlsx)")
+    else:
+        with tempfile.TemporaryDirectory() as tmpdir:
+            # 儲存 PDF
+            cdf_path = os.path.join(tmpdir, cdf_file.name)
+            # Save the uploaded file to cdf_path
+            with open(cdf_path, "wb") as f:
+                f.write(cdf_file.getbuffer())
+            download_buttons = []
+            
+            word_output_name = cdf_file.name.replace(".xlsx", f".docx")
+            output_path = os.path.join(tmpdir, word_output_name)
+            doc = cdf.run(cdf_path)
+            doc.save(output_path)
+
+            with open(output_path, "rb") as out_file:
+                st.download_button(
+                    label=f"下載 Word 檔",
+                    data=out_file,
+                    file_name=word_output_name,
+                    mime="application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+                )
