32361a3b241bccdff25a0e5561a844892cb56471
WANGICHEN <149982770+WANGICHEN@users.noreply.github.com>
2025-10-19 19:43:34 +0800

    Update bsmi.py

--- bsmi.py
+++ bsmi.py
@@ -19,7 +19,8 @@ def clean_str(x):
     return "" if s.lower() in {"nan", "none"} else s
 
 def comp_translation(comp, comp_df):
-    result = comp_df[comp_df['english'].str.lower() == comp.lower()]
+    result = comp_df[comp_df['english'] == comp]
+    result = comp_df[comp_df['english'].str.contains(comp, case=False, na=False)]
     if result.empty:
         return comp
     else:
@@ -133,6 +134,7 @@ def run(cdf_path):
     cdf_df.to_excel(output, index=False)
     output.seek(0)
     return output
+
 
 
 
